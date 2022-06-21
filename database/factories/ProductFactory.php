ae7a7b5a1fe486cf14d536b4b057ca0cbcb5dd28
Felipe Passos <felipepassos08@gmail.com>
2022-06-21 18:28:36 -0300

    Populando o banco de dados

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word(),
+            'cover' => $this->faker->imageUrl(),
+            'price' => $this->faker->randomFloat(1, 10, 30),
+            'description' => $this->faker->sentence(),
+            'stock' => $this->faker->randomDigit(),
+        ];
+    }
+}
